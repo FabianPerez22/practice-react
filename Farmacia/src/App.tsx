23a3c4c6cc58d2aff27ebc43742142a6a07413fd
FabianKP22 <fabiankretszchmann@gmail.com>
2025-11-27 09:00:23 -0400

    feat: install typeScript

--- Farmacia/src/App.tsx
+++ Farmacia/src/App.tsx
@@ -1,13 +1,13 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-
+  let veamos = "hola mundo";
 
   return (
     <>
       <h1>Hola mundo</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
